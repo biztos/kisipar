1cd62f28139524e445099e0168be9765762a4fb2
Kevin Frost <biztos@mac.com>
2017-02-15 12:33:59 +0100

    binsanity instead of bindata, yay testing!

--- refresh.sh
+++ refresh.sh
@@ -5,7 +5,7 @@
 # The version is the date, since every update needs its own version and there
 # end up being a lot of them; however it must match this regex:
 # (?:^(?!-)[a-z\d\-]{0,62}[a-z\d]$)
-go-bindata -pkg kisipar -prefix bindata bindata/... \
+binsanity bindata \
     && sed -i .bak "s/^LAST_REFRESH =.*/LAST_REFRESH = \"`date +%F-%H%M%S`\"/" version.go \
     && /bin/rm version.go.bak \
     && echo REFRESHED || echo FAILED
